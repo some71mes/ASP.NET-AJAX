f7a0f7e60196da80a5156b501e033d68f4ff6242
some71mes <andreymatosian94@gmail.com>
2026-09-23 16:00:09 +0300

    f

--- aspnetWebApp (v3)/obj/Debug/net10.0/aspnetWebApp.AssemblyInfo.cs
+++ aspnetWebApp (v3)/obj/Debug/net10.0/aspnetWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspnetWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de71e655b2a966614622c7ae18512166353743ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c508a0a3fed826cda35101f76d4b787888e7af1")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspnetWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aspnetWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
